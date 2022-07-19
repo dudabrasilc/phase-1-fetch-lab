9cd3ca93db526e5b8fb2998472d6614e93830c6a
Maria Coitinho <brasildu@gmail.com>
2022-07-19 14:42:12 -0500

    commit tree ecdd4eb7d4201d9e870f6bf3d948183e2ec8516a

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((response => response.json()))
+  .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
@@ -15,3 +16,56 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+// const arrays = [['duda', 'vlad'], ['mae', 'pai']];
+
+// function merge() {
+//     for (let i = 0; i < arrays.length; i++) {
+//     console.log(arrays.toString(", "))
+//     }
+//   }
+
+// merge()
+
+
+fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((data) => {
+    const result = data.forEach(obj => {
+          const arrays = Object.values(obj.characters);
+          console.log(arrays)
+          for (let i = 0; i < arrays.length; i++) {
+              return arrays.toString(", ");
+          }    
+        }
+      )
+    }
+  )       
+
+
+// the 1031st character of the series is the 597th from the second object from the data array
+// fetch("https://anapioficeandfire.com/api/books")
+//   .then((resp) => resp.json())
+//   .then(data => {
+//     data.find(obj => {
+//       const arrays = Object.values(obj.characters)
+//       console.log(arrays)
+//     })
+//   })
+
+
+fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then(array => {
+    // console.log(array)
+      let result = 0;
+      for (const obj of array) {
+        // debugger;
+        // console.log(obj.numberOfPages)
+        result += obj.numberOfPages;
+        // debugger;
+        // return result;
+      }
+      console.log(result);
+    })
